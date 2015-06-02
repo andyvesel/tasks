c8f01d414fd6c9cb813393694a2987a03788830e
Andrey <andyvesel@ya.ru>
2015-06-02 10:28:10 +0300

    Finish password generator

--- password_generator.rb
+++ password_generator.rb
@@ -1,12 +1,23 @@
-print 'Set password length (not less than 8 symbols): '
+loop do
+  print 'Set password length (not less than 8 symbols): '
 
-length = gets.to_i
+  @length = gets.to_i
 
-vowels = [:a, :e, :i, :o, :u, :y]
-consonants = [:b, :c, :d, :f, :g, :h, :j, :k, :l, :m, :n, :p, :q, :r, :s, :t, :v, :w, :x, :z]
+  # определил 2 массива: гласные и согласные буквы
+  @vowels = [:a, :e, :i, :o, :u, :y]
+  @consonants = [:b, :c, :d, :f, :g, :h, :j, :k, :l, :m, :n, :p, :q, :r, :s, :t, :v, :w, :x, :z]
 
+  def generate_password
+    @length.times.each do
+      @alphabet = [@vowels.sample, @consonants.sample].sample(1).join
+      print @alphabet
+    end
+  end
 
-length.times.each do
-  alphabet = [vowels.sample, consonants.sample].sample(2).join
-  print alphabet
+  if @length >= 8
+    generate_password
+    exit
+  else
+    puts 'Wrong choice, try again'
+  end
 end
