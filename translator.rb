b89e2a3106c023f8aedc245301be3eeec3b1566d
Andrey <andyvesel@ya.ru>
2015-06-09 16:39:51 +0300

    Add translator.rb

---
+++ translator.rb
@@ -0,0 +1,18 @@
+require 'net/https'
+require 'uri'
+
+print 'Enter word to translate: '
+
+word = gets.strip
+
+uri = URI('https://translate.yandex.net/api/v1.5/tr/translate')
+
+params = { :key => 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
+           :text => "#{word}",
+           :lang => 'ru'
+          }
+
+uri.query = URI.encode_www_form(params)
+
+response = Net::HTTP.get_response(uri)
+puts uri
