8a7b6f38ea0d6882ee34306fcc28978e455816f6
Andrey <andyvesel@ya.ru>
2015-06-10 13:22:56 +0300

    Refactoring hash params

--- translator.rb
+++ translator.rb
@@ -5,14 +5,14 @@ print 'Enter word to translate: '
 
 word = gets.strip
 
-uri = URI('https://translate.yandex.net/api/v1.5/tr/translate')
+uri = URI.parse('https://translate.yandex.net/api/v1.5/tr/translate')
 
-params = { :key => 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
-           :text => "#{word}",
-           :lang => 'ru'
+params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
+           text: "#{word}",
+           lang: 'ru'
           }
 
 uri.query = URI.encode_www_form(params)
 
 response = Net::HTTP.get_response(uri)
-puts uri
+puts response
