984548a5b7da25e67b9d8d20d694a9c2d2bc10a4
Andrey <andyvesel@ya.ru>
2015-06-18 10:49:21 +0300

    Update translator.rb

--- translator.rb
+++ translator.rb
@@ -5,6 +5,14 @@ print 'Enter word to translate: '
 
 word = gets.strip
 
+
+  # uri = URI.parse('https://translate.yandex.net/api/v1.5/tr/detect')
+
+  # params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
+  #            text: "#{word}",
+  #           }
+
+
 uri = URI.parse('https://translate.yandex.net/api/v1.5/tr/translate')
 
 params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
@@ -15,4 +23,11 @@ params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeee
 uri.query = URI.encode_www_form(params)
 
 response = Net::HTTP.get_response(uri)
-puts response.body
+
+f = File.open('translate.xml', "w") { |f| f.write("#{response.body}")}
+
+f = File.open('translate.xml').read
+
+f.each_line do |line|
+  print line
+end
