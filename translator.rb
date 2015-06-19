1b75523b77abd0998abd8738e217346085c9cec0
Andrey <andyvesel@ya.ru>
2015-06-19 15:33:52 +0300

    Add blocks to program

--- translator.rb
+++ translator.rb
@@ -1,33 +1,43 @@
 require 'net/https'
 require 'uri'
+require 'rexml/document'
+  include REXML
 
-print 'Enter word to translate: '
+print 'Enter your text: '
 
-word = gets.strip
+@word = gets.strip
 
-
-  # uri = URI.parse('https://translate.yandex.net/api/v1.5/tr/detect')
-
-  # params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
-  #            text: "#{word}",
-  #           }
-
-
-uri = URI.parse('https://translate.yandex.net/api/v1.5/tr/translate')
-
-params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
-           text: "#{word}",
-           lang: 'ru'
-          }
-
-uri.query = URI.encode_www_form(params)
-
-response = Net::HTTP.get_response(uri)
-
-f = File.open('translate.xml', "w") { |f| f.write("#{response.body}")}
-
-f = File.open('translate.xml').read
-
-f.each_line do |line|
-  print line
+def language_detect
+  @uri = URI.parse('https://translate.yandex.net/api/v1.5/tr/detect')
+  @params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
+              text: "#{@word}", 
+            }
 end
+
+def translate
+  @uri = URI.parse('https://translate.yandex.net/api/v1.5/tr/translate')
+  @params = { key: 'trnsl.1.1.20150602T124834Z.3912e53ab6807e56.55924160b82c19beeeea211cd149031e2e2623ef',
+              text: "#{@word}",
+              lang: 'ru'
+            }
+end
+
+def get_response
+  @uri.query = URI.encode_www_form(@params)
+  @response = Net::HTTP.get_response(@uri)
+end
+
+def text_to_file
+  @f = File.open('translate.xml', 'w') { |f| f.write("#{@response.body}")}
+  @f = File.open('translate.xml').read
+end
+
+def show_text
+  doc = Document.new(@f)
+  doc.elements.each("Translation/text") { ||e print e.text }
+end
+
+translate
+get_response
+text_to_file
+show_text
