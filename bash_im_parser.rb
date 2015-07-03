33009f134c2c6bdc7dea9b190e07ed1e71a470c1
Andrey <andyvesel@ya.ru>
2015-07-03 11:48:45 +0300

    Add json gem

--- bash_im_parser.rb
+++ bash_im_parser.rb
@@ -1,6 +1,7 @@
 require 'hpricot'
 require 'open-uri'
+require 'json'
 
 output = Hpricot(open('http://bash.im'))
 
-puts output.search("//div[@class='text']").inner_html
+puts output.search("//div[@class='text']").inner_html.to_json
